995292ca2ba98edcfcce49346e5b0dd66de8ec2c
Elchanan Haas <elchanan@amazon.com>
2026-03-01 12:40:01 -0800

    Switch to LocalSet

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use crate::{
     agents::agent_helper::Agent,
     card::IRONCLAD_ATTACK_CARDS,
     ui::{
-        fight_ui::UIState,
         ui_actor::{UIActor, UIEvent},
     },
 };
@@ -11,9 +10,8 @@ use agents::mcts_agent::MctsAgent;
 use agents::random_agent::RandomAgent;
 use futures::StreamExt;
 use game::{Charachter, Game};
-use ratatui::widgets::Widget;
 use rng::Rng;
-use tokio::sync::mpsc::{self, Sender};
+use tokio::{sync::mpsc::{self, Sender}, task::{LocalSet, spawn_local}};
 
 mod act;
 mod agents;
@@ -29,14 +27,17 @@ mod rng;
 mod ui;
 mod util;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(4)
+    let local_set = LocalSet::new();
+    let runtime = tokio::runtime::Builder::new_current_thread()
         .enable_all()
-        .build()?
-        .block_on(async { agent_play().await })
+        .build()?;
+    local_set.block_on(&runtime, agent_play())?;
+    Ok(())
 }
 
-fn human_play() {}
+fn human_play() {
+    todo!()
+}
 
 async fn agent_play() -> Result<(), Box<dyn std::error::Error>> {
     let game = Game::new(Charachter::IRONCLAD);
@@ -49,21 +50,24 @@ async fn agent_play() -> Result<(), Box<dyn std::error::Error>> {
     let (sender, receiver) = mpsc::channel(8);
     setup_keystream(sender.clone());
     let mut ui_actor = UIActor::new(receiver);
-    tokio::spawn(async move { ui_actor.run().await });
+    let ui_handle = spawn_local(async move { ui_actor.run().await });
     while !choice.is_over() {
-        //This errors if the display is closed. In this case, exit the program.
+        //This returns Err if the display is closed. In this case, exit the program.
         let Ok(_) = sender.send(UIEvent::NewState(choice.clone())).await else {
             break;
         };
         agent.take_action(&mut choice, &mut rng);
     }
+    //The UI actor has some code to restore terminal settings on drop. This 
+    //join ensures it will be run. It already has a panic hook by default.
+    ui_handle.await.expect("UI exited");
     //print!("{}", &choice);
     Ok(())
 }
 
 fn setup_keystream(sender: Sender<UIEvent>) {
-    let mut reader = ratatui::crossterm::event::EventStream::new();
-    tokio::spawn(async move {
+    let mut reader = crossterm::event::EventStream::new();
+    spawn_local(async move {
         while let Some(x) = reader.next().await {
             let event = x.expect("The crossterm event stream isn't broken.");
             let Ok(_) = sender.send(UIEvent::KeyPress(event)).await else {
--- src/ui/fight_ui.rs
+++ src/ui/fight_ui.rs
@@ -7,37 +7,30 @@ use fliptui::widgets::{BorderWidget, TextRegion, text_line};
 use crate::game::Game;
 use crate::game::choice::{ChoiceState, RestSiteAction};
 
-pub struct UIState<'a> {
-    choice_state: &'a ChoiceState,
-}
+type FResult = Result<(), std::fmt::Error>;
 
-impl<'a> UIState<'a> {
-    pub fn new(choice_state: &'a ChoiceState) -> Self {
-        Self { choice_state }
-    }
-}
-
-fn render_player(widget: &mut Widget, state: &ChoiceState) {
+fn render_player(widget: &mut Widget, state: &ChoiceState) -> FResult {
     let game = state.game();
     let mut widget = BorderWidget::builder(widget).build();
     text_line(&mut widget.title, "Player");
     let mut text_region = TextRegion::new(&mut widget.center);
-    writeln!(&mut text_region, "{}", game.charachter().name());
+    writeln!(&mut text_region, "{}", game.charachter().name())?;
     writeln!(
         &mut text_region,
         "{}/{} hp",
         game.player_hp(),
         game.player_max_hp()
-    );
-    writeln!(&mut text_region, "{} energy", game.fight().energy());
-    writeln!(&mut text_region, "{} block", game.fight().player_block());
+    )?;
+    writeln!(&mut text_region, "{} energy", game.fight().energy())?;
+    writeln!(&mut text_region, "{} block", game.fight().player_block())?;
     if let Some(position) = game.act().position {
-        writeln!(&mut text_region, "{} block", game.fight().player_block());
-        writeln!(&mut text_region, "floor {}", position.y);
+        writeln!(&mut text_region, "{} block", game.fight().player_block())?;
+        writeln!(&mut text_region, "floor {}", position.y)?;
     }
+    Ok(())
 }
 
-fn render_card(widget: &mut Widget, state: &ChoiceState, card_idx: usize) {
+fn render_card(widget: &mut Widget, state: &ChoiceState, card_idx: usize) -> FResult {
     let game = state.game();
     let card = game.fight().hand().get(card_idx);
     let mut widget = BorderWidget::builder(widget).build();
@@ -45,66 +38,70 @@ fn render_card(widget: &mut Widget, state: &ChoiceState, card_idx: usize) {
     if let Some(card) = card {
         let upgraded = if card.is_upgraded() { "+" } else { "" };
         if let Some(cost) = game.fight().evaluate_cost(card) {
-            write!(&mut text_region, "{:?}{} [{}]", card.body, upgraded, cost);
+            write!(&mut text_region, "{:?}{} [{}]", card.body, upgraded, cost)?;
         } else {
-            write!(&mut text_region, "{:?}{}", card.body, upgraded);
+            write!(&mut text_region, "{:?}{}", card.body, upgraded)?;
         };
     }
+    Ok(())
 }
 
-fn render_cards(widget: &mut Widget, state: &ChoiceState) {
+fn render_cards(widget: &mut Widget, state: &ChoiceState) -> FResult {
     widget.layout().push_grid_template_row_fr(1.0);
     for i in 0..Game::MAX_CARDS_IN_HAND {
         widget.layout().push_grid_template_column_fr(1.0);
         let mut child = widget.child();
         child.layout().grid_col(i).grid_row(0);
-        render_card(&mut child, state, i);
+        render_card(&mut child, state, i)?;
     }
+    Ok(())
 }
 
-fn render_enemy(widget: &mut Widget, state: &ChoiceState, enemy_idx: usize) {
+fn render_enemy(widget: &mut Widget, state: &ChoiceState, enemy_idx: usize) -> FResult {
     let game = state.game();
     let enemy = &game.fight().enemies().enemies[enemy_idx];
     let Some(enemy) = enemy else {
-        return;
+        return Ok(());
     };
     let mut widget = BorderWidget::builder(widget).build();
     let mut text_region = TextRegion::new(&mut widget.center);
-    writeln!(&mut text_region, "{}", enemy.name);
-    writeln!(&mut text_region, "{}/{} hp", enemy.hp, enemy.max_hp);
+    writeln!(&mut text_region, "{}", enemy.name)?;
+    writeln!(&mut text_region, "{}/{} hp", enemy.hp, enemy.max_hp)?;
     if enemy.block > 0 {
-        writeln!(&mut text_region, "{} block", enemy.block);
+        writeln!(&mut text_region, "{} block", enemy.block)?;
     }
     if enemy.buffs.strength > 0 {
-        writeln!(&mut text_region, "{} str", enemy.buffs.strength);
+        writeln!(&mut text_region, "{} str", enemy.buffs.strength)?;
     }
     if enemy.buffs.ritual > 0 || enemy.buffs.ritual_skip_first > 0 {
         writeln!(
             &mut text_region,
             "{} ritual",
             enemy.buffs.ritual + enemy.buffs.ritual_skip_first
-        );
+        )?;
     }
     if enemy.buffs.curl_up > 0 {
-        writeln!(&mut text_region, "{} curl up", enemy.buffs.curl_up);
+        writeln!(&mut text_region, "{} curl up", enemy.buffs.curl_up)?;
     }
     if enemy.debuffs.vulnerable > 0 {
-        writeln!(&mut text_region, "{} vulnerable", enemy.debuffs.vulnerable);
+        writeln!(&mut text_region, "{} vulnerable", enemy.debuffs.vulnerable)?;
     }
     if enemy.debuffs.weak > 0 {
-        writeln!(&mut text_region, "{} weak", enemy.debuffs.weak);
+        writeln!(&mut text_region, "{} weak", enemy.debuffs.weak)?;
     }
+    Ok(())
 }
 
-fn render_enemies(widget: &mut Widget, state: &ChoiceState) {
+fn render_enemies(widget: &mut Widget, state: &ChoiceState) -> FResult {
     //TODO - this should have a more clever layout.
     widget.layout().push_grid_template_row_fr(1.0);
     for i in 0..Game::MAX_ENEMIES {
         widget.layout().push_grid_template_column_fr(1.0);
         let mut child = widget.child();
         child.layout().grid_col(i).grid_row(0);
-        render_enemy(&mut child, state, i);
+        render_enemy(&mut child, state, i)?;
     }
+    Ok(())
 }
 
 fn vertical_breakdown<'a>(widget: &mut Widget<'a>) -> [Widget<'a>; 3] {
@@ -125,21 +122,21 @@ fn render_rest_site(
     widget: &mut Widget,
     _choice_state: &ChoiceState,
     rest_site_actions: Vec<RestSiteAction>,
-) {
+) -> FResult {
     let [_top, mut middle, _bottom] = vertical_breakdown(widget);
     middle.layout().flex_direction(taffy::FlexDirection::Row)
                    .justify_content(taffy::AlignContent::Center)
                    .align_items(taffy::AlignItems::Center);
-    const MAX_RESTSITE_ACTIONS: usize = 5;
     for i in 0..rest_site_actions.len() {
         let mut child = widget.child();
         child.layout().border_left_px(1).border_top_px(1).border_right_px(1).border_bottom_px(1);
         let mut text_region = TextRegion::new(&mut child);
-        writeln!(&mut text_region, "{:?}", rest_site_actions[i]);
+        writeln!(&mut text_region, "{:?}", rest_site_actions[i])?;
     }
+    Ok(())
 }
 
-fn render_battlefield(widget: &mut Widget, choice_state: &ChoiceState) {
+fn render_battlefield(widget: &mut Widget, choice_state: &ChoiceState) -> FResult {
     let [_top, mut middle, mut bottom] = vertical_breakdown(widget);
     middle.layout().push_grid_template_row_fr(1.0);
     let [mut player_box, mut fight_box] = array::from_fn(|i| {
@@ -148,18 +145,19 @@ fn render_battlefield(widget: &mut Widget, choice_state: &ChoiceState) {
             w.layout().grid_col(i).grid_row(0);
         })
     });
-    render_player(&mut player_box, choice_state);
-    render_cards(&mut bottom, choice_state);
-    render_enemies(&mut fight_box, choice_state);
+    render_player(&mut player_box, choice_state)?;
+    render_cards(&mut bottom, choice_state)?;
+    render_enemies(&mut fight_box, choice_state)?;
+    Ok(())
 }
 
 pub fn draw_ui(widget: &mut Widget, choice_state: &ChoiceState) {
     match choice_state.choice().clone() {
         crate::game::choice::Choice::PlayCardState(play_card_actions) => {
-            render_battlefield(widget, choice_state)
+            let _ = render_battlefield(widget, choice_state);
         }
         crate::game::choice::Choice::ChooseEnemyState(choose_enemy_actions, _) => {
-            render_battlefield(widget, choice_state)
+            let _ = render_battlefield(widget, choice_state);
         }
         crate::game::choice::Choice::Win => {}
         crate::game::choice::Choice::Loss => {}
@@ -175,7 +173,7 @@ pub fn draw_ui(widget: &mut Widget, choice_state: &ChoiceState) {
         crate::game::choice::Choice::TransformCardState(transform_card_actions) => {}
         crate::game::choice::Choice::UpgradeCardState(upgrade_card_actions) => {}
         crate::game::choice::Choice::RestSite(rest_site_actions) => {
-            render_rest_site(widget, choice_state, rest_site_actions);
+            let _ = render_rest_site(widget, choice_state, rest_site_actions);
         }
     }
 }
